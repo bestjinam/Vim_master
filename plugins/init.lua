0c496cdc75e162d4a285ac9b465458c62596e6b2
bestjinam <jinam@studetn.42seoul.kr>
2023-09-03 14:34:02 +0900

    init config

---
+++ plugins/init.lua
@@ -0,0 +1,18 @@
+return {
+  -- plugins = {
+  --   init = { 
+  --     -- You can also add new plugins here as well:
+  --     -- Add plugins, the packer syntax without the "use"
+  --     -- Custom colorscheme dracula plugin
+  --   },
+  -- },
+
+  -- Set colorscheme to use
+  colorscheme = "dracula",
+
+  { "42Paris/42header", lazy = false },
+  { "Mofiqul/dracula.nvim" },
+  { "Diogo-ss/42-header.nvim" },
+  { "42Paris/42header", lazy = false },
+  { "Mofiqul/dracula.nvim" },
+}
